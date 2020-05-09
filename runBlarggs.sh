ff576a0f25e37a1efa6c68928855de73bac4f5d6
Marshall Gates <mggates@gmail.com>
2020-05-09 16:51:30 -0400

    Zoey has paging so now the combined will work

--- runBlarggs.sh
+++ runBlarggs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "RUNNING BLARRG'S CPU INSTRUCTION EXECUTION TEST ROMS!"
-# ./main --rom="roms/gb-test-roms/cpu_instrs/cpu_instrs.gb"
-# if [ $? -ne 0 ]
-# then
+./main --rom="roms/gb-test-roms/cpu_instrs/cpu_instrs.gb"
+if [ $? -ne 0 ]
+then
   ls roms/gb-test-roms/cpu_instrs/individual | while read line
   do 
     ./main --rom="roms/gb-test-roms/cpu_instrs/individual/${line}"
   done
-# fi
+fi
 echo
 echo "RUNNING BLARRG'S CPU INSTRUCTION TIMING TEST ROMS!"
 ./main --rom="roms/gb-test-roms/instr_timing/instr_timing.gb"
